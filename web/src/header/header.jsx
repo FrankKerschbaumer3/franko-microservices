5a5805c33342f74edad622ad08b756b045170127
Edge, Matthew <mattedgeis@gmail.com>
2018-02-20 17:07:05 -0500

    Slight fix to header for Link elements

--- web/src/header/header.jsx
+++ web/src/header/header.jsx
@@ -9,13 +9,11 @@ const Header = () => (
       <div className="header-item header-logo">
         <div className="">T</div>
       </div>
-      <div className="header-item header-button is-site-header-item-selected">
-        <Link to="/">Home</Link>
-      </div>
+      <Link className="header-item header-button is-site-header-item-selected" to="/">Home</Link>
     </div>
     <div className="header-section">
-      <div className="header-item header-button">Settings</div>
-      <div className="header-item header-button">Log out</div>
+      <Link className="header-item header-button" to="/settings">Settings</Link>
+      <Link className="header-item header-button" to="/logout">Logout</Link>
     </div>
   </div>
 )
