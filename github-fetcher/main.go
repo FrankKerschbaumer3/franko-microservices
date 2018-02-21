26137370a7c70d3fe9b8401344a5c5aae250f1de
Edge, Matthew <mattedgeis@gmail.com>
2018-02-21 13:17:22 -0500

    GitHub fetcher RPC prototype

---
+++ github-fetcher/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"google.golang.org/grpc"
+	"golang.org/x/net/context"
+)
+
+type issueFetcher struct {}
+
+func (*issueFetcher) FetchIssues(context.Context, *FetchReq) (*FetchReply, error) {
+	return &FetchReply{Issues:nil}, nil
+}
+
+func main() {
+	port := flag.Int("port", 13500, "RPC server port")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	RegisterIssueFetcherServer(grpcServer, &issueFetcher{})
+	grpcServer.Serve(lis)
+}
